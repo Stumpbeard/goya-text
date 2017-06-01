8b42eff5b353335d881a2c1c3a823f2764ee4ff8
Stump <stumpbeard@gmail.com>
2017-05-31 23:10:09 -0400

    Make the urls pretty when HTML5 is available

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ angular.module('myApp', [
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  $locationProvider.html5Mode(true);
 
   $routeProvider.otherwise({redirectTo: '/'});
 }]).
