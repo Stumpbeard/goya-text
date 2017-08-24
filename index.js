4a1d79d2c6989d191fcaecad33151f1e3e908fb9
Stump <stumpbeard@gmail.com>
2017-08-23 23:20:14 -0400

    Remove emojis

--- index.js
+++ index.js
@@ -152,5 +152,6 @@ function escapeHtml(unsafe) {
         .replace(/>/g, "")
         .replace(/"/g, "")
         .replace(/\[/g, "")
-        .replace(/]/g, "");
+        .replace(/]/g, "")
+        .replace(/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g, '');
 }
